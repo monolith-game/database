d5695a15efc9e7dd63ce067a479108449ce46954
Chris Norman <chris.norman2@googlemail.com>
2023-08-23 12:42:04 +0100

    Finish tests.

--- test/src/daos/utils_dao_test.dart
+++ test/src/daos/utils_dao_test.dart
@@ -1,13 +1,17 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:test/test.dart';
 
+import '../../common.dart';
 import '../../get_database.dart';
 
 void main() {
+  final random = Random();
   final database = getDatabase();
   final serverProfilesDao = database.serverProfilesDao;
   final serverSecurityContextsDao = database.serverSecurityContextsDao;
+  final soundsDao = database.soundsDao;
   final utilsDao = database.utilsDao;
 
   group(
@@ -16,28 +20,41 @@ void main() {
       test(
         '.getServerProfileContext',
         () async {
-          final insecure = await serverProfilesDao.createServerProfile(
-            name: 'Test',
-            host: '0.0.0.0',
-            port: 8080,
+          await runUntilCount(
+            f: (final i) async {
+              final securityContext = random.nextBool()
+                  ? await serverSecurityContextsDao.createServerSecurityContext(
+                      chainFile: File('fullchain$i.pem'),
+                      keyFile: File('privkey$i.pem'),
+                    )
+                  : null;
+              final mainMenuMusic = random.nextBool()
+                  ? await soundsDao.createSound('main_music_$i.mp3')
+                  : null;
+              final menuSelectSound = random.nextBool()
+                  ? await soundsDao.createSound('menu_select_$i.mp3')
+                  : null;
+              final menuActivateSound = random.nextBool()
+                  ? await soundsDao.createSound('menu_activate$i.mp3')
+                  : null;
+              final profile = await serverProfilesDao.createServerProfile(
+                name: 'Profile $i',
+                host: '0.0.0.0',
+                port: i,
+                securityContext: securityContext,
+                mainMenuMusic: mainMenuMusic,
+                menuSelectSound: menuSelectSound,
+                menuActivateSound: menuActivateSound,
+              );
+              final context =
+                  await utilsDao.getServerProfileContext(profile.id);
+              expect(context.mainMenuMusic, mainMenuMusic);
+              expect(context.menuActivateSound, menuActivateSound);
+              expect(context.menuSelectSound, menuSelectSound);
+              expect(context.securityContext, securityContext);
+              expect(context.serverProfile, profile);
+            },
           );
-          var context = await utilsDao.getServerProfileContext(insecure.id);
-          expect(insecure.securityContextId, null);
-          expect(context.serverProfile, insecure);
-          final securityContext =
-              await serverSecurityContextsDao.createServerSecurityContext(
-            chainFile: File('fullchain.pem'),
-            keyFile: File('privkey.pem'),
-          );
-          final secure = await serverProfilesDao.createServerProfile(
-            name: 'Secure',
-            host: '0.0.0.0',
-            port: 8080,
-            securityContext: securityContext,
-          );
-          context = await utilsDao.getServerProfileContext(secure.id);
-          expect(context.securityContext, securityContext);
-          expect(context.serverProfile, secure);
         },
       );
     },
